09e923c5518c26f7162860a53378b3cd503d4122
guillaume Rousseau <igolus@gmail.com>
2017-04-19 15:01:10 +0200

    iamges + tag docs

--- src/main/java/org/vaadin/easyapp/util/annotations/ContentView.java
+++ src/main/java/org/vaadin/easyapp/util/annotations/ContentView.java
@@ -8,15 +8,37 @@ import static java.lang.annotation.RetentionPolicy.RUNTIME;
 
 @Retention(RUNTIME)
 public @interface ContentView {
-
+	/**
+	 * Used to tell that the icon is not set
+	 */
 	public static final String NOT_SET = "NOT_SET";
-	//public static final Class<?>  = ContentView.
+	
+	/**
+	 * define the how the view should be displayed
+	 */
 	String viewName();
+	
+	/**
+	 * icon to display in the navigation panel could FontAwesom can be used 
+	 */
 	String icon() default NOT_SET;
+	
+	/**
+	 * int to define the sorting of the elements
+	 */
 	int sortingOrder() default 0;
+	/**
+	 * set to true if the view should be displayed as startup
+	 */
 	boolean homeView() default false;
+	/**
+	 * Root view where this view should be displayed
+	 */
 	Class<?> rootViewParent() default EasyAppMainView.class;
-	//default means sot set
+
+	/**
+	 * Parent view 
+	 */
 	Class<?> componentParent() default EasyAppMainView.class;
 }
 
--- src/main/java/org/vaadin/easyapp/util/annotations/RootView.java
+++ src/main/java/org/vaadin/easyapp/util/annotations/RootView.java
@@ -5,8 +5,20 @@ import static java.lang.annotation.RetentionPolicy.RUNTIME;
 
 @Retention(RUNTIME)
 public @interface RootView {
+	/**
+	 * Used to tell that the icon is not set
+	 */
 	public static final String NOT_SET = "NOT_SET";
+	/**
+	 * define the how the view should be displayed
+	 */
 	String viewName();
+	/**
+	 * icon to display in the navigation panel could FontAwesom can be used 
+	 */
 	String icon() default NOT_SET;
+	/**
+	 * int to define the sorting of the elements
+	 */
 	int sortingOrder() default 0;
 }
